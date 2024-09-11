0b223632c2817f19806cadd0b6e4801f0a1fe6e6
Leslie Liu <s9172051212@gmail.com>
2024-09-11 22:29:02 +0000

    edit auto8

--- hw2_rsc/test_unsortedarray.py
+++ hw2_rsc/test_unsortedarray.py
@@ -4,8 +4,9 @@ This module contains unit tests for the merge_sort function with a un-sorted arr
 
 from hw2_debugging import merge_sort
 
+
 def test_merge_sort():
     """
     Test cases for merge_sort function with a un-sorted array as input.
     """
-    assert(merge_sort([3, 1, 4, 2, 5]) == [1, 2, 3, 4, 5])
+    assert (merge_sort([3, 1, 4, 2, 5]) == [1, 2, 3, 4, 5])
