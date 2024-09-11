ee9d7e9001688ad363c5ef5df28f6f28428113aa
Leslie Liu <s9172051212@gmail.com>
2024-09-11 22:09:17 +0000

    edit test case

--- hw2_rsc/test_unsortedarray.py
+++ hw2_rsc/test_unsortedarray.py
@@ -1,21 +1,11 @@
 """
-This module contains unit tests for the merge_sort function with a reversed array.
+This module contains unit tests for the merge_sort function with a un-sorted array.
 """
-import unittest
+
 from hw2_debugging import merge_sort
 
-
-class TestMergeSort(unittest.TestCase):
+def test_merge_sort():
     """
-    Test cases for merge_sort function with a unsorted array as input.
+    Test cases for merge_sort function with a un-sorted array as input.
     """
-
-    def test_reverse_sorted_array(self):
-        """
-        This module for merge sort use unsorted array as input.
-        """
-        self.assertEqual(merge_sort([3, 1, 4, 2, 5]), [1, 2, 3, 4, 5])
-
-
-if __name__ == '__main__':
-    unittest.main()
+    assert(merge_sort([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5])
