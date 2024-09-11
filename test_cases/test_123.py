276437cf8224450e48c9fd6fe955134eddf95919
Leslie Liu <s9172051212@gmail.com>
2024-09-11 23:12:29 +0000

    move test case to other folder

---
+++ test_cases/test_123.py
@@ -0,0 +1,12 @@
+"""
+This module contains unit tests for the merge_sort function with a reversed array.
+"""
+from hw2_rsc.hw2_debugging import merge_sort
+
+
+def test_merge_sort():
+    """
+    Test cases for merge_sort function with a reverse-sorted array as input.
+    """
+    assert (merge_sort([5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5])
+    
